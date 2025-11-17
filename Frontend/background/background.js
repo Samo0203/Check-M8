1f8506f2cf175562a75285a3914a783ea85b1946
Samodya <samodya0203samadini@gmail.com>
2025-11-17 17:29:54 +0530

    Delete Frontend/background directory

--- Frontend/background/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("✅ CheckM8 installed!");
-});
-
-chrome.action.onClicked.addListener((tab) => {
-  console.log("🧩 Extension clicked on:", tab.url);
-});
